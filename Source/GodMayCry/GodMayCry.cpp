a5b4268184f51ee42443b013524c7417bb0eea29
ryoichihasegawa <ryoichi20010901@gmail.com>
2022-10-03 01:52:24 +0900

    add fale
    ue4プロジェクトファイル一式移した

---
+++ Source/GodMayCry/GodMayCry.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GodMayCry.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GodMayCry, "GodMayCry" );
